ca1e2abe7a4a1568fb46e020e6d44bc60821e653
Rodnei Couto <rodneicouto@Rodneis-MacBook-Pro.local>
2017-07-07 16:24:10 -0300

    Commit Inicial - Vamos que vamos pessoal!!!!!

---
+++ src/app/index.module.js
@@ -0,0 +1,14 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('app', [ 
+        'ngAnimate', 
+        'ngCookies', 
+        'ngSanitize', 
+        'ui.router', 
+        'ui.bootstrap', 
+        'toastr'
+    ]);
+
+})();
